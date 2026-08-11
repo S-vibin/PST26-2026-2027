662a22276d8b7c78c4c5c5eed2113089a6328af4
VIBIN  S <sr.vibinvibin2005@gmail.com>
2026-08-11 10:37:20 +0530

    Add files via upload

---
+++ methodWOrtWarg.java
@@ -0,0 +1,11 @@
+public class methodWOrtWarg {
+    public void addValue(int a,int b){
+        int sum=a+b;
+        System.out.println(sum);
+    }
+    public static void main(String[] args) {
+       methodWOrtWarg obj=new methodWOrtWarg();
+       obj.addValue(8, 7); 
+    }
+    
+}
