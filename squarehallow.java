9a099878ede7a92b40c92aa0112cd32bcfca71c3
VIBIN  S <sr.vibinvibin2005@gmail.com>
2026-08-12 22:27:59 +0530

    Add files via upload

---
+++ squarehallow.java
@@ -0,0 +1,17 @@
+public class squarehallow {
+    public static void main(String[] args) {
+        int a=6;
+        for(int i=1;i<=a;i++){
+            for(int j=1;j<=a;j++){
+                if(i==1 || i==a || j==1 || j==a){
+
+                System.out.print(":P");
+              } 
+              else{
+                System.out.print("  ");
+                 
+            }
+        }
+         System.out.println();
+    }
+}}
