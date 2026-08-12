06b1a285b90c115e0f815fc40f6e08ef30f1b96a
VIBIN  S <sr.vibinvibin2005@gmail.com>
2026-08-12 22:25:38 +0530

    Add files via upload

---
+++ pttern.java
@@ -0,0 +1,29 @@
+public class pttern {
+    public static void main(String[] args) {
+        int a=7;
+        for(int i=1;i<=a;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(":P"+" ");
+                
+            }System.out.println();
+        }
+         for(int k=a;k>=1;k--){
+            for(int m=k;m>=1;m--){
+                System.out.print(":P"+" ");
+                
+            }System.out.println();
+        }
+        for(int r=a;r>=1;r++){
+            for(int t=1;t>=r;t++){
+                System.out.print(":P"+" ");
+                
+            }System.out.println();
+        }
+         for(int y=1;y<=a;y--){
+            for(int u=y;u<=1;u--){
+                System.out.print(":P"+" ");
+                
+            }System.out.println();
+        }
+    }
+}
