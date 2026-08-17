87ec7be171a208036e6c09a03d35e24555b0f694
VIBIN  S <sr.vibinvibin2005@gmail.com>
2026-08-17 11:33:27 +0530

    Add files via upload

---
+++ secondelement_arraylist.java
@@ -0,0 +1,16 @@
+
+import java.util.ArrayList;
+
+public class secondelement_arraylist {
+    public static void main(String[] args) {
+        ArrayList<String> list=new ArrayList<>();
+       list.add("rule");
+       list.add("no1");
+       list.add("never");
+       list.add("talk");
+       list.add("about");
+       list.add("fightclub");
+       
+       System.out.println("shhhh!"+" "+list.get(5));
+    }
+}
