db5c1271ba7d8c3967f3771beae4fb4a36c7cd57
VIBIN  S <sr.vibinvibin2005@gmail.com>
2026-08-17 10:21:43 +0530

    Add files via upload

---
+++ ArrayList_method.java
@@ -0,0 +1,15 @@
+
+import java.util.ArrayList;
+
+public class ArrayList_method {
+    public static void main(String[] args) {
+       ArrayList<String> list=new ArrayList<>();
+       list.add("he");
+       list.add("is");
+       list.add("insane");
+       list.addFirst("ummaiva");
+       System.out.print(list); 
+    }
+    
+    
+}
