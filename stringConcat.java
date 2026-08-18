94e83f49b543f7ce406198b6405bb985181e9458
VIBIN  S <sr.vibinvibin2005@gmail.com>
2026-08-18 11:28:19 +0530

    Add files via upload

---
+++ stringConcat.java
@@ -0,0 +1,7 @@
+public class stringConcat {
+    public static void main(String[] args) {
+        String s=new String("hi");
+        s=s.concat("chello");
+        System.out.println(s);
+    }
+}
