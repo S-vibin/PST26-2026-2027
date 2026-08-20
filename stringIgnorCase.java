f52f1226bcd111f135796ea0d4303e305920dd1b
VIBIN  S <sr.vibinvibin2005@gmail.com>
2026-08-20 21:09:29 +0530

    Add files via upload

---
+++ stringIgnorCase.java
@@ -0,0 +1,8 @@
+public class stringIgnorCase {
+    
+    public static void main(String[] args) {
+        String str="I like trains";
+        boolean ig=str.equalsIgnoreCase("i Like trains");
+        System.out.println("ignor the stuff"+" "+ig);
+}
+}
